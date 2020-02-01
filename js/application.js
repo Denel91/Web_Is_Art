7a798e684ea36856560ff7646b21be284bb61119
Daniele Di Salvo <disi91@hotmail.it>
2020-02-01 21:06:21 +0100

    INSERT js folder

---
+++ js/application.js
@@ -0,0 +1,21 @@
+/**
+ * Registra un utente sul local storage del Browser
+ * */
+function clientRegister() {
+
+    localStorage.utente = JSON.stringify({
+        username: document.getElementById("inputUsername").value,
+        email: document.getElementById("inputEmail").value,
+        password: document.getElementById("inputPassword").value
+    });
+
+    var utente = JSON.parse(localStorage.utente);
+
+    alert("Benvenuto " + utente.username);
+
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem("utente", JSON.stringify(utente))
+}
